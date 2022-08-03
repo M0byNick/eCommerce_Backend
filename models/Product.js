8d035dfcdde9c81114cf5279ed6b1305b3818a56
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-08-03 00:42:20 -0400

    Product Tag model

--- models/Product.js
+++ models/Product.js
@@ -16,7 +16,7 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-    productName: {
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
@@ -27,7 +27,7 @@ Product.init(
         isDecimal: true
       }
     },
-    inventoryStock: {
+    inventory_stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
@@ -35,7 +35,7 @@ Product.init(
         isNumeric: true
       }
     },
-    categoryId: {
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
